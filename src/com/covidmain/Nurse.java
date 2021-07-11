107b95585cc2a344dbabda62c9fd1878a57e4b3f
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2021-07-11 20:22:51 +0530

    Updated Nurse function
    Used string concatenation

--- src/com/covidmain/Nurse.java
+++ src/com/covidmain/Nurse.java
@@ -1,42 +1,42 @@
 package com.covidmain;
-import java.sql.*;
+
 import java.util.*;
 
+//function to generate nurse id
+class nurseID {
+    static String nurseID = "";
 
-class nurseID
-{
-    static long nurse_id;
-    //function to generate nurse id
-    static long nurseIDgenerator()
-    {
-        try
-        {
-            Class.forName("oracle.jdbc.driver.OracleDriver"); //class loader function
-            System.out.println("Driver Loaded!");
-			
-			Connection connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle123");
-			System.out.println("Connection established!");
-			Statement stmt = connect.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT COUNT(N_ID) FROM nurse");
-            while (rs.next()) 
-            {
-                nurse_id = rs.getInt(1);
-            }
-            connect.close();
+    static String nurseIDgenerator() {
+        long nurseCount = NurseSQL.getNurseCount();
+        System.out.println("NurseCount: " + nurseCount);
 
-            return nurse_id;
+        String nurseCountStr = "";
+        if (nurseCount >= 0 && nurseCount < 10) {
+            nurseCountStr = "0000" + String.valueOf(nurseCount);
         } 
-        catch (Exception e) 
-        {
-            System.out.println(e);
-            return -1;
+        else if(nurseCount >= 10 && nurseCount < 100) {
+            nurseCountStr = "000" + String.valueOf(nurseCount);
         }
+        else if(nurseCount >= 100 && nurseCount < 1000) {
+            nurseCountStr = "00" + String.valueOf(nurseCount);
+        }
+        else if(nurseCount >= 1000 && nurseCount < 10000) {
+            nurseCountStr = "0" + String.valueOf(nurseCount);
+        }
+        
+        if (nurseCount == -1) {
+            System.out.println("Error in fetching database!");
+        } else {
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+            nurseID = 'N' + String.valueOf(year) + nurseCountStr;
+        }
+
+        return nurseID;
     }
 }
 
-public class Nurse extends nurseID 
-{
-    long N_id;
+public class Nurse extends nurseID {
+    String N_id;
     String N_name;
     int N_slot;
     String N_phone;
@@ -44,10 +44,9 @@ public class Nurse extends nurseID
     String N_mail;
     char N_status;
 
-    //default constructor
-    Nurse()
-    {
-        this.N_id = 0;
+    // default constructor
+    Nurse() {
+        this.N_id = "";
         this.N_name = "";
         this.N_slot = 0;
         this.N_phone = "";
@@ -58,85 +57,74 @@ public class Nurse extends nurseID
 
     Scanner sc = new Scanner(System.in);
 
-    //setters
-    void setNurseID()
-    {
-        this.N_id = nurseIDgenerator();
+    // setters
+    void setNurseID() {
+        String tempN_ID = nurseIDgenerator();
+        if (tempN_ID == "") {
+            System.out.println("Error!! Can't set Nurse ID");
+        } else {
+            this.N_id = tempN_ID;
+        }
     }
 
-    void setNurseName()
-    {
+    void setNurseName() {
         sc.nextLine();
         System.out.print("\nEnter Name        : ");
         String nursename = sc.nextLine();
         this.N_name = nursename;
     }
 
-    void setNurseSlot()
-    {
+    void setNurseSlot() {
         System.out.print("\nEnter slot no.    : ");
         int slot_no = sc.nextInt();
         this.N_slot = slot_no;
     }
 
-    void setNursePhone()
-    {
+    void setNursePhone() {
         sc.nextLine();
         System.out.print("\nEnter Mobile No    : ");
         String nurseMob = sc.nextLine();
-        if(checkers.mobileChecker(nurseMob))
-        {
+        if (checkers.mobileChecker(nurseMob)) {
             this.N_name = nurseMob;
-        }
-        else
-        {
+        } else {
             System.out.println("Invalid Mobile No.!!!");
             System.out.print("Please Re-");
             setNursePhone();
         }
-        
     }
 
-    void setNurseAdd()
-    {
+    void setNurseAdd() {
         sc.nextLine();
         System.out.print("\nEnter Address     : ");
         String nurseadd = sc.nextLine();
         this.N_add = nurseadd;
     }
 
-    void setNurseMail()
-    {
+    void setNurseMail() {
         sc.nextLine();
         System.out.print("\nEnter E-mail     : ");
         String nursemail = sc.nextLine();
-        if(checkers.emailChecker(nursemail))
-        {
+        if (checkers.emailChecker(nursemail)) {
             this.N_mail = nursemail;
-        }
-        else
-        {
+        } else {
             System.out.println("Invalid mail!!");
-            System.out.print("Please ReEnter Mail-");
+            System.out.print("Please Re-");
             setNurseMail();
         }
-        
     }
 
-    void setNurseStatus()
-    {
+    void setNurseStatus() {
         sc.nextLine();
         System.out.print("\nEnter Nursstatus     : ");
         char nursestatus = sc.next().charAt(0);
         this.N_status = nursestatus;
-        //TODO Adding checker for entered status
+        // TODO Adding checker for entered status
     }
 
-    //TODO -getters
+    // TODO -getters
 
-    //function to add new nurse
-    void addNurse()
-    {
+    // function to add new nurse
+    void addNurse() {
         Nurse N1 = new Nurse();
         N1.setNurseID();
         N1.setNurseName();
@@ -146,29 +134,27 @@ public class Nurse extends nurseID
         N1.setNurseMail();
         N1.setNurseStatus();
 
-        if(NurseSQL.addNewNurse(N1))
-        {
+        if (NurseSQL.addNewNurse(N1)) {
             System.out.println("New nurse added successfully!!");
-        }
-        else
-        {
+        } else {
             System.out.println("Caution! Some error occured!!");
         }
     }
 
-    //function to remove nurse
-    void removeNurse(){
-        //here we don't have to remove nurse details from database;
-        //only change status to n
+    // function to remove nurse
+    void removeNurse() {
+        // here we don't have to remove nurse details from database;
+        // only change status to n
+
     }
 
-    void detailNurse(){}
-
+    void detailNurse() {
+    }
 
     // void attendenceNurse(){}
 
     public static void main(String[] args) {
-        System.out.print("\nTemp. Count: " + nurseIDgenerator());
+        System.out.print("\nTemp ID: " + nurseIDgenerator());
     }
 
 }
--- src/com/covidmain/NurseSQL.java
+++ src/com/covidmain/NurseSQL.java
@@ -2,7 +2,33 @@ package com.covidmain;
 import java.sql.*;
 
 
-public class NurseSQL {
+public class NurseSQL 
+{
+    static long getNurseCount()
+    {
+    	long nurse_id = 0;
+        try
+        {
+            Class.forName("oracle.jdbc.driver.OracleDriver"); //class loader function
+            System.out.println("Driver Loaded!");
+			Connection connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle123");
+			System.out.println("Connection established!");
+			Statement stmt = connect.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT COUNT(N_ID) FROM nurse");
+            while (rs.next()) 
+            {
+                nurse_id = rs.getInt(1);
+            }
+            connect.close();
+
+            return nurse_id;
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e);
+            return -1;
+        }
+    }
 
     public static boolean addNewNurse(Nurse N) 
     {
@@ -21,5 +47,6 @@ public class NurseSQL {
             return false;
         }
     }
+    
 
 }
