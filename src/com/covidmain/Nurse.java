040bebca864e766963eecfd44a0aea7e62e6dbe3
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2021-07-15 23:08:48 +0530

    Modified and corrected nurse
    Corrected nurseID generator, corrected remove nurse function, modified add new nurse function (Yet to be corrected)

--- src/com/covidmain/Nurse.java
+++ src/com/covidmain/Nurse.java
@@ -1,40 +1,64 @@
 package com.covidmain;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 //function to generate nurse id
-class nurseID {
+class nurseHelper
+{
+	static Dbhelper db = new Dbhelper();
+	static ResultSet rs = null;
+	
+	static long getNurseCount()
+    {
+    	long nurse_id = 0;
+    	String statement = "SELECT COUNT(N_ID) as Count FROM nurse";
+        try 
+        {
+        	db.startstatement();
+        	rs = db.execstatement(statement);
+        	rs.next();
+		    nurse_id=rs.getInt("Count");
+		    return nurse_id;
+		}
+        catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		      return -1;    		      
+        }
+    }
+	
+	//NURSE_ID
     static String nurseID = "";
 
     static String nurseIDgenerator() {
-        long nurseCount = NurseSQL.getNurseCount();
-//        System.out.println("NurseCount: " + nurseCount);
+        long nurseCount = NurseSQL.getNurseCount() + 1;
+        System.out.println("NurseCount: " + nurseCount);
         String nurseCountStr = "";
         if (nurseCount >= 0 && nurseCount < 10) {
-            nurseCountStr = "0000" + String.valueOf(nurseCount);
+            nurseCountStr = "00" + String.valueOf(nurseCount);
         } 
         else if(nurseCount >= 10 && nurseCount < 100) {
-            nurseCountStr = "000" + String.valueOf(nurseCount);
+            nurseCountStr = "0" + String.valueOf(nurseCount);
         }
         else if(nurseCount >= 100 && nurseCount < 1000) {
-            nurseCountStr = "00" + String.valueOf(nurseCount);
-        }
-        else if(nurseCount >= 1000 && nurseCount < 10000) {
-            nurseCountStr = "0" + String.valueOf(nurseCount);
+            nurseCountStr = "" + String.valueOf(nurseCount);
         }
         
         if (nurseCount == -1) {
             System.out.println("Error in fetching database!");
-        } else {
+        } 
+        else {
             int year = Calendar.getInstance().get(Calendar.YEAR);
             nurseID = 'N' + String.valueOf(year) + nurseCountStr;
         }
 
         return nurseID;
     }
+    
 }
 
-public class Nurse extends nurseID {
+public class Nurse extends nurseHelper {
     String N_id;
     String N_name;
     int N_slot;
@@ -69,7 +93,6 @@ public class Nurse extends nurseID {
     }
 
     void setNurseName() {
-        sc.nextLine();
         System.out.print("\nEnter Name        : ");
         String nursename = sc.nextLine();
         this.N_name = nursename;
@@ -82,27 +105,25 @@ public class Nurse extends nurseID {
     }
 
     void setNursePhone() {
-        sc.nextLine();
+    	sc.nextLine();
         System.out.print("\nEnter Mobile No    : ");
         String nurseMob = sc.nextLine();
         if (checkers.mobileChecker(nurseMob)) {
             this.N_name = nurseMob;
         } else {
             System.out.println("Invalid Mobile No.!!!");
-            System.out.print("Please Re-");
+            System.out.print("Press Re-");
             setNursePhone();
         }
     }
 
     void setNurseAdd() {
-        sc.nextLine();
         System.out.print("\nEnter Address     : ");
         String nurseadd = sc.nextLine();
         this.N_add = nurseadd;
     }
 
     void setNurseMail() {
-        sc.nextLine();
         System.out.print("\nEnter E-mail     : ");
         String nursemail = sc.nextLine();
         if (checkers.emailChecker(nursemail)) {
@@ -115,7 +136,6 @@ public class Nurse extends nurseID {
     }
 
     void setNurseStatus() {
-        sc.nextLine();
         System.out.print("\nEnter Nurse status (A/N)   : ");
         char nursestatus = sc.next().charAt(0);
         if(nursestatus == 'A' || nursestatus == 'N')
@@ -129,24 +149,25 @@ public class Nurse extends nurseID {
         }
     }
 
-    // TODO -getters
+     // TODO -getters
 
     // function to add new nurse
-    void addNurse() {
-        Nurse N1 = new Nurse();
-        N1.setNurseID();
-        N1.setNurseName();
-        N1.setNurseSlot();
-        N1.setNursePhone();
-        N1.setNurseAdd();
-        N1.setNurseMail();
-        N1.setNurseStatus();
-
-        if (NurseSQL.addNewNurse(N1)) {
-            System.out.println("New nurse added successfully!!");
-        } else {
-            System.out.println("Caution! Some error occured!!");
-        }
+    static void addNurse() {
+        Nurse N = new Nurse();
+        N.setNurseID();
+        N.setNurseName();
+        N.setNurseSlot();
+        N.setNursePhone();
+        N.setNurseAdd();
+        N.setNurseMail();
+        N.setNurseStatus();
+        
+        String statement = "INSERT INTO nurse(n_id, n_name, n_slot, n_phone, n_add, n_mail, isremoved, n_status) VALUES('" + N.N_id + "','" + N.N_name + "'," + N.N_slot + ",'" + N.N_phone + "','" + N.N_add + "','" + N.N_mail + "','" + N.isRemoved + "','" + N.N_status + "')";
+        db.startstatement();
+        db.update(statement);
+        
+        System.out.println("New nurse added successfully!! ID: " + N.N_id);
+        
     }
 
     // function to remove nurse TODO testing
@@ -155,21 +176,60 @@ public class Nurse extends nurseID {
         // only change status to N
         System.out.println("**** Remove Nurse ****");
         System.out.print("\nEnter Nurse ID    : ");
-        sc.nextLine();
-        String nurseID = sc.nextLine();
-        NurseSQL.removeNurseRec(nurseID);
+        String tempNurseID = sc.nextLine();
+        
+        String statement = "UPDATE nurse SET isremoved = 'Y' WHERE N_ID = '" + tempNurseID +"'";
+        
+        db.startstatement();
+        db.update(statement);
+//		db.endupdate();
+       
     }
 
-    void detailNurse() {
+    public static void displayNurseDetails() throws SQLException {
+		System.out.println("** Display Nurse Details **");
+        System.out.print("\nEnter Nurse ID    : ");
+        String tempNurseID = sc.nextLine();
 
+        String statement = "SELECT * FROM nurse WHERE N_ID = '" + tempNurseID +"'";
+        try 
+        {
+        	db.startstatement();
+        	rs = db.execstatement(statement);
+		}
+        catch(Exception e) { 
+            e.printStackTrace();
+            System.out.println("Error! or Records not exist");
+            System.out.println(e);
+        }      
+        while(rs.next()){
+            System.out.println("Nurse ID		: " + rs.getString("N_ID"));
+            System.out.println("Nurse Name		: " + rs.getString("N_name"));
+            System.out.println("Nurse Slot 		: " + rs.getInt("N_slot"));
+            System.out.println("Nurse Phone No. : " + rs.getString("N_phone"));
+            System.out.println("Nurse Add. 		: " + rs.getString("N_add"));
+            System.out.println("Nurse E-mail	: " + rs.getString("N_mail"));
+            System.out.println("Nurse Removed?	: " + rs.getString("N_removed"));
+            System.out.println("Nurse Status    : " + rs.getString("N_status"));
+         }
+        db.endstatement();
+        
+	}
+    
+    static void detailNurseSlotWise()
+    {
+        System.out.println("\nSlot wise nurse details");
+        
     }
-
-    // void attendenceNurse(){}
 
     //main function for testing 
-    public static void main(String[] args) {
-        System.out.print("\nRunning ");
-        Nurse.removeNurse();
+    public static void main(String[] args) throws SQLException {
+        System.out.println("\nRunning ");
+//        System.out.println(nurseIDgenerator());
+//        Nurse.displayNurseDetails();
+//        Nurse.removeNurse();
+        	Nurse.addNurse();
+        
     }
 
 }
--- src/com/covidmain/checkers.java
+++ src/com/covidmain/checkers.java
@@ -1,15 +1,13 @@
 package com.covidmain;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class checkers {
-    static boolean emailChecker(String inputMail)
-    {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                            "[a-zA-Z0-9_+&*-]+)*@" +
-                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                            "A-Z]{2,7}$";
-                              
+    static boolean emailChecker(String inputMail) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z"
+                + "A-Z]{2,7}$";
+
         Pattern pat = Pattern.compile(emailRegex);
 
         if (inputMail == null)
