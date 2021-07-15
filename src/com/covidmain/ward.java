c761822b480fa6aaf1b05f1ef907247366235ad2
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2021-07-15 23:04:43 +0530

    initiated ward

---
+++ src/com/covidmain/ward.java
@@ -0,0 +1,10 @@
+package com.covidmain;
+
+public class ward {
+    char wardNo;       //A, B, C, D
+    char wardType;       //E/N
+    int wardCapacity;    //total
+    int noBeds;         //engage
+
+
+}
