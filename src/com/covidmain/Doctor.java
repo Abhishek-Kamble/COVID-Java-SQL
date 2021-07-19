7b64f5a93f53f9caac680e641a6e6536063bcc92
shital29-s <shitalsolanke39@gmail.com>
2021-07-19 15:25:46 +0530

    Doctor class updated

--- src/com/covidmain/Doctor.java
+++ src/com/covidmain/Doctor.java
@@ -2,75 +2,97 @@ package com.covidmain;
 import java.sql.*;
 import java.util.*;
 
-
-class DoctorID{
-	
-	long Doctor_id;
-	
-	long DoctorIDgenerator(){
+class Doctorhelper{
+		static Dbhelper db = new Dbhelper();
+		static ResultSet rs = null;
 		
-		try
+		
+	static long getDoctorCount()
+    {
+    	long doctor_id = 0;
+    	String statement = "SELECT COUNT(D_ID) as Count FROM Doctor";
+        try 
         {
-            Class.forName("oracle.jdbc.driver.OracleDriver"); 
-			System.out.print("Driver Loaded Successfully.. ");
-    
-		
-			Connection connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "shital2901"); 
-			System.out.println("Database connected Successfully!\n");
-			
-			Statement stmt = connect.createStatement();
-
-			
-			ResultSet rs = stmt.executeQuery("SELECT COUNT(D_ID) FROM Doctor");
-
-            
-            while (rs.next()) 
-            {
-                Doctor_id = rs.getInt(1);
-            }
-            connect.close();
+        	db.startstatement();
+        	rs = db.execstatement(statement);
+        	rs.next();
+		    doctor_id=rs.getInt("Count");
+		    return doctor_id;
+		}
+        catch(SQLException se){
+		      
+		      se.printStackTrace();
+		      return -1;    		      
+        }
+    }
+	static String doctorID = "";
+	
+    static String DoctorIDgenerator() 
+    {
+        long doctorCount = DoctorSQL.getDoctorCount() + 1;
+        System.out.println("doctorCount: " + doctorCount);
+        
+        String doctorCountStr = "";
+        
+        if (doctorCount >= 0 && doctorCount < 10) {
+            doctorCountStr = "00" + String.valueOf(doctorCount);
+        }else 
+        	if(doctorCount >= 10 && doctorCount < 100) {
+            doctorCountStr = "0" + String.valueOf(doctorCount);
+        }else 
+        	if(doctorCount >= 100 && doctorCount < 1000) {
+            doctorCountStr = "" + String.valueOf(doctorCount);
+        }
+        
+        if (doctorCount == -1) {
+            System.out.println("Error in fetching database!");
         } 
-        catch (Exception e) 
-        {
-            System.out.println(e);
+        else {
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+            doctorID = 'D' + String.valueOf(year) + doctorCountStr;
         }
 
-        return Doctor_id;
-	}
+        return doctorID;
+    }
+    
 }
 
-public class Doctor extends DoctorID {
-    long D_id;
+public class Doctor extends Doctorhelper {
+    String D_id;
     String D_name;
     int D_slot;
     String D_phone;
     String D_add;
     String D_mail;
     char D_status;
-    String D_dept;
-    String D_Tperiod;
-    String D_tperiod;
+    char isRemoved;
 
-    Doctor()
-    {
-        this.D_id = 0;
-        this.D_name = "";
-        this.D_slot = 0;
-        this.D_phone = "";
-        this.D_add = "";
-        this.D_mail = "";
-        this.D_status = 'N';
-        this.D_dept = "";
-        this.D_Tperiod = "";
-        this.D_tperiod = "";
-      }
+    Doctor() 
+	{
+		this.D_id = "";
+		this.D_name = "";
+		this.D_slot = 0;
+		this.D_phone = "";
+		this.D_add = "";
+		this.D_mail = "";
+		this.D_status = 'N';
+        this.isRemoved = 'N';
+	}
 
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
-    void setDoctorID()
-    {
-        this.D_id = DoctorIDgenerator();
-    }
+	void setDoctorID() 
+	{
+	    String tempD_ID = DoctorIDgenerator();
+	    if (tempD_ID == "") 
+	    {
+	        System.out.println("Error!! Can't set Doctor ID");
+	    } 
+	    else 
+	    {
+	    	this.D_id = tempD_ID;
+	    }
+	}
 
     void setDoctorName()
     {
@@ -80,109 +102,160 @@ public class Doctor extends DoctorID {
         this.D_name = Dname;
     }
 
-    void setDoctorSlot()
+    void setDoctorSlot() 
     {
-        System.out.print("\nEnter slot no.    		: ");
-        int Dslot_no = sc.nextInt();
-        this.D_slot = Dslot_no;
+        System.out.print("\nEnter slot no.    : ");
+        int slot_no = sc.nextInt();
+        this.D_slot = slot_no;
     }
 
-    void setDoctorDept()
+    void setDoctorPhone() 
     {
-        sc.nextLine();
-        System.out.print("\nEnter Department        : ");
-        String Dept = sc.nextLine();
-        this.D_dept = Dept;
-    }
-    void setDoctorPhone()
-    {
-        sc.nextLine();
+    	sc.nextLine();
         System.out.print("\nEnter Mobile No    : ");
-        String DocMob = sc.nextLine();
-        if(checkers.mobileChecker(DocMob))
+        String doctorMob = sc.nextLine();
+        if (checkers.mobileChecker(doctorMob))
         {
-            this.D_phone = DocMob;
-        }
-        else
+            this.D_name = doctorMob;
+        } 
+        else 
         {
-            System.out.println("Invalid Mobile No.!!");
-            System.out.print("Please Re Enter -");
+            System.out.println("Invalid Mobile No.!!!");
+            System.out.print("Press Re-");
             setDoctorPhone();
         }
-        
     }
 
-    void setDoctorAdd()
-    {
-        sc.nextLine();
-        System.out.print("\nEnter Address     : ");
-        String Docadd = sc.nextLine();
-        this.D_add = Docadd;
-    }
+	    void setDoctorAdd() 
+	    {
+	        System.out.print("\nEnter Address     : ");
+	        String doctoradd = sc.nextLine();
+	        this.D_add = doctoradd;
+	    }
 
-    void setDoctorMail()
-    {
-        sc.nextLine();
-        System.out.print("\nEnter E-Mail     : ");
-        String Docemail = sc.nextLine();
-        if(checkers.emailChecker(Docemail))
-        {
-            this.D_mail = Docemail;
-        }
-        else
-        {
-            System.out.println("Invalid mail!!");
-            System.out.print("Please Re enter your mail -");
-            setDoctorMail();
-        }
-        
-    }
+	    void setDoctorMail() 
+	    {
+	    	System.out.print("\nEnter E-mail     : ");
+	    	String doctormail = sc.nextLine();
+	    	if (checkers.emailChecker(doctormail)) {
+	    		this.D_mail = doctormail;
+	    	} 
+	    	else 
+	    	{
+	    		System.out.println("Invalid mail!!");
+	    		System.out.print("Please Re-");
+	    		setDoctorMail();
+	    	}
+	    }
+	    void setDoctorStatus() {
+	        System.out.print("\nEnter Doctor status (A/N)   : ");
+	        char doctorstatus = sc.next().charAt(0);
+	        if(doctorstatus == 'A' || doctorstatus == 'N')
+	        {
+	            this.D_status = doctorstatus;
+	        }
+	        else
+	        {
+	            System.out.print("\n!Re-");
+	            setDoctorStatus();
+	        }
+	    }
 
-    void setDoctorStatus()
-    {
-        sc.nextLine();
-        System.out.print("\nEnter Doctor status (trainee(T)/temp(t)/permanent(p)  : ") ;
-        char Docstatus = sc.next().charAt(0);
-        this.D_status = Docstatus;
-        if(Docstatus == 'N'|| Docstatus != 'T' || Docstatus != 't' || Docstatus != 'p' )
-        {
-        	System.out.println("\nInvalid Status Entered     !!");
-        	System.out.println("\nPlease ReEnter -      !!");
-        	setDoctorStatus();
-        }
-        if(Docstatus == 'T')
-        {
-        	System.out.println("\nEnter training period       : ");
-            String D_Training = sc.nextLine();
-            this.D_Tperiod = D_Training;
-        }
-        else if(Docstatus == 't')
-        {
-        	System.out.println("\nEnter Working period        : ");
-        	String D_tWork = sc.nextLine();
-        	this.D_tperiod = D_tWork;
-        }
-    }
+	    static void addDoctor() 
+	    {
+			    Doctor D = new Doctor();
+			    D.setDoctorID();
+			    D.setDoctorName();
+			    D.setDoctorSlot();
+			    D.setDoctorPhone();
+			    D.setDoctorAdd();
+			    D.setDoctorMail();
+			    D.setDoctorStatus();
+		    
+			    String statement = "INSERT INTO Doctor(d_id, d_name, d_slot, d_phone, d_add, d_mail,isRemoved, d_status) VALUES ('" + D.D_id + "','" + D.D_name + "'," + D.D_slot + ",'" + D.D_phone + "','" + D.D_add + "','" + D.D_mail + "','" + D.isRemoved + "','" + D.D_status + "')";
+			    db.startstatement();
+			    db.update(statement);
+			    
+			    System.out.println("New Doctor added successfully!! ID: " + D.D_id);
+	    }
+	    
+	    static void removeDoctor() 
+	    {
+	        System.out.println("**** Remove Doctor ****");
+	        System.out.print("\nEnter Doctor ID    : ");
+	        String tempDoctorID = sc.nextLine();
+	        
+	        String statement = "UPDATE Doctor SET isremoved = 'Y' WHERE D_ID = '" + tempDoctorID +"'";
+	        
+	        db.startstatement();
+	        db.update(statement);
+			db.endupdate();
+			
+			System.out.println("-------------------------------------------------");
+	    }
+	    
+	    public static void displayDoctorDetails() throws SQLException
+	    {
+			System.out.println("** Display Doctor Details **");
+	        System.out.print("\nEnter Doctor ID    : ");
+	        String tempDoctorID = sc.nextLine();
 
-    void addDoctor()
-    {
-        Doctor D1 = new Doctor();
-        D1.setDoctorID();
-        D1.setDoctorName();
-        D1.setDoctorSlot();
-        D1.setDoctorPhone();
-        D1.setDoctorAdd();
-        D1.setDoctorMail();
-        D1.setDoctorStatus();
-        D1.setDoctorDept();
+	        String statement = "SELECT d_id, d_name, d_slot, d_phone, d_add, d_mail , isremoved, d_status FROM Doctor WHERE D_ID = '" + tempDoctorID + "'";
 
-        if(DoctorSQL.addNewDoctor(D1))
-        {
-            System.out.println("New Doctor added successfully!!");
-        }
-        else
-        {
-            System.out.println("Some error occured!!");
-        }
-    }
+	    	db.startstatement();
+	    	rs = db.execstatement(statement);
+	    	while(rs.next()){
+	            System.out.println("Doctor ID		: " + rs.getString("D_ID"));
+	            System.out.println("Doctor Name		: " + rs.getString("D_name"));
+	            System.out.println("Doctor Slot 		: " + rs.getInt("D_slot"));
+	            System.out.println("Doctor Phone No. : " + rs.getString("D_phone"));
+	            System.out.println("Doctor Add. 		: " + rs.getString("D_add"));
+	            System.out.println("Doctor E-mail	: " + rs.getString("D_mail"));
+	            System.out.println("Doctor Removed?	: " + rs.getString("isremoved"));
+	            System.out.println("Doctor Status    : " + rs.getString("D_status"));
+	         }
+	        db.endstatement(); 
+			
+	        System.out.println("-----------------------------------------------");
+		}
+	    
+	    
+	    static void changeDoctorSlot()
+	    {
+	    	System.out.println("***Change Doctor Slot***");
+	    	System.out.print("Enter Doctor ID: ");
+	        String tempDoctorID = sc.nextLine();
+	        System.out.print("\nEnter new Slot No. for"+ tempDoctorID + " : ");
+	        int tempSlot = sc.nextInt();
+	        
+	        String statement = "UPDATE Doctor SET D_slot = " + tempSlot + "WHERE D_ID = '" + tempDoctorID +"'";
+	        
+	        db.startstatement();
+	        db.update(statement);
+	        db.endstatement();
+	        System.out.println("Slot No. changed successfully for Doctor ID: " + tempDoctorID);
+	    }
+	    static void detailDoctorSlotWise()
+	    {
+	    	 System.out.println("\n***Slot wise Doctor list***");
+	         System.out.print("\nEnter Slot No.    : ");
+	         int tempSlot = sc.nextInt();
+	         String statement = "SELECT D_id, D_name FROM Doctor WHERE D_slot = '" + tempSlot + "'";
+	         
+	     	db.startstatement();
+	     	db.printDataList(statement);
+	         db.endstatement();
+	         
+	         System.out.println("-----------------------------------------------");
+	     }   
+	     public static void main(String[] args) throws SQLException
+	     {
+	         System.out.println("\nRunning ");
+	       System.out.println(DoctorIDgenerator());
+	        Doctor.displayDoctorDetails();
+            //Doctor.removeDoctor();
+	    	//Doctor.addDoctor();
+	       // Doctor.detailDoctorSlotWise();
+	       // Doctor.changeDoctorSlot();
+	    }
 }
