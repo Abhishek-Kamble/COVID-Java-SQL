c6af92ec269c520a829e7c4e2535c428a2c2be2d
shital29-s <shitalsolanke39@gmail.com>
2021-07-22 17:57:36 +0530

    updated

--- src/com/covidmain/patient_at_entry.java
+++ src/com/covidmain/patient_at_entry.java
@@ -6,148 +6,125 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
-class patientID {
+class patientHelper
+{
+	static Dbhelper db = new Dbhelper();
+	static ResultSet rs = null;
 	
-	long patient_id;
+	static long getPatientCount() throws SQLException
+    {
+    	long patient_id = 0;
+    	String statement = "SELECT COUNT(P_ID) as Count FROM patient_at_entry";
+        
+    	db.startstatement();
+    	rs = db.execstatement(statement);
+        rs.next();
+		patient_id=rs.getInt("Count");
+		return patient_id;
+    }
 	
-	long patientIDgenerator()
-	{
-		try
-        {
-            Class.forName("oracle.jdbc.driver.OracleDriver"); 
-			System.out.print("Driver Loaded Successfully.. ");
-    
-			
-			Connection connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "shital2901"); 
-			System.out.println("Database connected Successfully!\n");
+    static String patientID = "";
 
-			
-			Statement stmt = connect.createStatement();
+    static String patientIDgenerator() throws SQLException 
+    {
+        long patientCount = getPatientCount() + 1;
 
-			
-			ResultSet rs = stmt.executeQuery("SELECT COUNT(P_ID) FROM patient_Entry_details");
-
-            
-            while (rs.next()) 
-            {
-                patient_id = rs.getInt(1);
-            }
-            connect.close();
+        String patientCountStr = "";
+        if (patientCount >= 0 && patientCount < 10) {
+            patientCountStr = "0000" + String.valueOf(patientCount);
         } 
-        catch (Exception e) 
-        {
-            System.out.println(e);
+        else if(patientCount >= 10 && patientCount < 100) {
+            patientCountStr = "000" + String.valueOf(patientCount);
+        }
+        else if(patientCount >= 100 && patientCount < 1000) {
+            patientCountStr = "00" + String.valueOf(patientCount);
+        }
+        else if(patientCount >= 1000 && patientCount < 10000) {
+            patientCountStr = "0" + String.valueOf(patientCount);
+        }
+        else if(patientCount >= 10000 && patientCount < 100000) {
+            patientCountStr = " " + String.valueOf(patientCount);
         }
 
-        return patient_id;
-		
-	}
-}
+        if (patientCount == -1) {
+            System.out.println("Error in fetching database!");
+        } 
+        else {
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+            patientID = 'P' + String.valueOf(year) + patientCountStr;
+        }
 
-public class patient_at_entry extends patientID{
-	long p_id;
+        return patientID;
+    }
+    
+    
+}
+public class patient_at_entry extends patientHelper
+{
+	String p_id;
 	String p_name;
 	int p_age;
 	String p_add;
 	String p_phone;
 	String p_mail;
-	char p_gender;
-	long p_adhar;
-	String p_app;
+	String p_adhar;
 	
 	patient_at_entry()
 	{
-		this.p_id = 0;
+		this.p_id = "";
         this.p_name = "";
         this.p_age = 0;
         this.p_phone = "";
         this.p_add = "";
-        this.p_mail = "";
-        this.p_gender = 'c';
-        this.p_adhar = 0;
-        this.p_app = "";
+        this.p_adhar = "";
 	}
 	
-	Scanner sc = new Scanner(System.in);
-	
-	void setpatientID()
-	{
-        this.p_id = patientIDgenerator();
-    }
+	static Scanner sc = new Scanner(System.in);
 
-    void setpatientName()
+    
+    void setPatientID() throws SQLException
     {
-        sc.nextLine();
-        System.out.print("\nEnter Name of the Patient     : ");
-        String patientname = sc.nextLine();
-        this.p_name = patientname;
-       
+        String tempP_ID = patientIDgenerator();
+        if (tempP_ID == "") {
+            System.out.println("Error!! Can't set Patient ID.");
+        } else {
+            this.p_id = tempP_ID;
+        }
     }
+	    void setPatientName() 
+	    {
+	        System.out.print("\nEnter Name        : ");
+	        String patientname = sc.nextLine();
+	        this.p_name = patientname;
+	    }
 	
-    void setpatientPhone()
-    {
-        sc.nextLine();
-        System.out.print("\nEnter Mobile No    : ");
-        String patientMob = sc.nextLine();
-        if(checkers.mobileChecker(patientMob))
-        {
-            this.p_phone = patientMob;
-        }
-        else
-        {
-            System.out.println("Invalid Mobile No.!!!");
-            System.out.print("Please Re Enter mobile no -");
-            setpatientPhone();
-        }
-    }
-        void setpatientAdd()
-        {
-            sc.nextLine();
-            System.out.print("\nEnter Address     : ");
-            String patientadd = sc.nextLine();
-            this.p_add = patientadd;
-        }
-        
-        void setpatientgen()
-        {
-            sc.nextLine();
-            System.out.print("\nEnter gender F/M    : ");
-            char patientGen = sc.next().charAt(0);
-            this.p_gender = patientGen;
-        }
-        
-        void setpatientMail()
-        {
-            sc.nextLine();
-            System.out.print("\nEnter Email     : ");
-            String patientmail = sc.nextLine();
-            if(checkers.emailChecker(patientmail))
-            {
-                this.p_mail = patientmail;
-            }
-            else
-            {
-                System.out.println("Invalid mail!!");
-                System.out.print("Please Re Enter-");
-                setpatientMail();
-            }
-        }
-        void setpatientAge()
-        {
-            sc.nextLine();
-            System.out.print("\nEnter age of the patient     : ");
-            int patientage= sc.nextInt();
-            this.p_age = patientage; 
-        }
-        void setAppointment()
-        {
-        	sc.nextLine();
-            System.out.print("\nEnter name of doctor for appointment : ");
-            String pAppoint= sc.nextLine();
-            this.p_app = pAppoint;
-        }
+	    void setPatientAge() {
+	        System.out.print("\nEnter Age of patient     : ");
+	        int PAge = sc.nextInt();
+	        this.p_age = PAge;
+	    }
+	
+	    void setPatientPhone() {
+	    	sc.nextLine();
+	        System.out.print("\nEnter Mobile No    : ");
+	        String patientMob = sc.nextLine();
+	        if (checkers.mobileChecker(patientMob)) {
+	            this.p_phone = patientMob;
+	        } else {
+	            System.out.println("Invalid Mobile No.!!!");
+	            System.out.print("Press ENTER!!");
+	            setPatientPhone();
+	        }
+	    }
+
+	    void setPatientAdd() {
+	        System.out.print("\nEnter Address     : ");
+	        String patientadd = sc.nextLine();
+	        this.p_add = patientadd;
+	    }
+
        
-        void setpatientAdhar()
+        void setPatientAdhar()
         {
             sc.nextLine();
             System.out.print("\nEnter adhar card no. of the patient     : ");
@@ -155,27 +132,59 @@ public class patient_at_entry extends patientID{
             this.p_adhar= patientadhar; 
         }
         
-        void Newpatient()
-        {
-        	patient_at_entry p1 = new patient_at_entry();
-            p1.setpatientID();
-            p1.setpatientName();
-            p1.setpatientAge();
-            p1.setpatientPhone();
-            p1.setpatientAdd();
-            p1.setpatientMail();
-            p1.setAppointment();
-            p1.setpatientgen();
-            p1.setpatientAdhar();
+        static void addPatient() throws SQLException {
+        	patient_at_entry P = new patient_at_entry();
+            P.setPatientID();
+            P.setPatientName();
+            P.setPatientAge();
+            P.setPatientPhone();
+            P.setPatientAdd();
+            P.setPatientAdhar();
+//            P.setPatientStatus();
+//            P.setAdmitDate();
+//            P.setDischargeDate();
+            String statement = "INSERT INTO patient_at_entry(P_id, P_name, P_age, P_phone, P_add , P_adhar) VALUES('" + P.p_id + "','" + P.p_name + "'," + P.p_age + ",'" + P.p_phone + ",'"  + P.p_add + "','" + P.p_adhar  + "')";
+            db.startstatement();
+            db.update(statement);
+  
+            System.out.println("New Patient added successfully!! ID: " + P.p_id);
             
-            if(patientSQL.addNewPatient(p1))
-            {
-                System.out.println("New patient added !!");
-            }
-            else
-            {
-                System.out.println("Error occured !! ");
-            }
         }
+        
+        
+        public static void displayPatientDetails() throws SQLException 
+        {
+    		System.out.println("** Display Patient Details **");
+            System.out.print("\nEnter Patient ID    : ");
+            String tempPatientID = sc.nextLine();
+
+            String statement = "SELECT p_id, p_name, p_phone, p_add, p_mail  FROM patient_at_entry WHERE P_ID = '" + tempPatientID + "'";
+
+        	db.startstatement();
+        	rs = db.execstatement(statement);
+        	while(rs.next()){
+                System.out.println("Patient ID		: " + rs.getString("p_ID"));
+                System.out.println("Patient Name		: " + rs.getString("p_name"));
+//                System.out.println("Patient Slot 		: " + rs.getInt("p_slot"));
+                System.out.println("Patient Phone No. : " + rs.getString("p_phone"));
+                System.out.println("Patient Add. 		: " + rs.getString("p_add"));
+                System.out.println("Patient Adhar	: " + rs.getString("p_adhar"));
+//                System.out.println("Patient Removed?	: " + rs.getString("isremoved"));
+//                System.out.println("Patient Status    : " + rs.getString("N_status"));
+             }
+            db.endstatement(); 
+    		
+            System.out.println("-----------------------------------------------");
+    	}
+        
+        public static void main(String[] args) throws SQLException 
+        {
+            System.out.println("\nRunning ");
+            System.out.println(patientIDgenerator());
+            patient_at_entry.addPatient();
+
+            
+        }
+        
     }
 
