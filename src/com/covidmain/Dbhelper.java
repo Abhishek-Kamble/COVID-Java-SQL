d09cdfab4160be769531a98668bcb50d6ddd7d27
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2021-07-28 14:59:45 +0530

    Updated startstatement function with back functions

--- src/com/covidmain/Dbhelper.java
+++ src/com/covidmain/Dbhelper.java
@@ -8,7 +8,7 @@ public class Dbhelper {
 	static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:XE";
 		
 	static final String USER = "system";
-	static final String PASS = "shital2901";
+	static final String PASS = "oracle123";
 	
 	// SQL Environment Setup
 	static Connection conn = null;
@@ -20,8 +20,10 @@ public class Dbhelper {
 		try {
 			Class.forName(JDBC_DRIVER);
 
-		    //STEP 3: Open a connection
-		    System.out.println("Connecting to database...\n");
+			System.out.print("Connecting to database...");
+	        Thread.sleep(1000); // Just to give the user a chance to see "hello".
+	        System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+	        System.out.println("                                                 \n");
 		    this.conn = DriverManager.getConnection(DB_URL,USER,PASS);
 			}catch(SQLException se){
 		      //Handle errors for JDBC
